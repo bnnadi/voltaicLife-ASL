5f2e07bc457c202b14d335c8fdeaccfef1a74981
Bisike <bisike.nnadi@gmail.com>
2013-09-16 19:55:05 -0400

    in app folder is where all my work will be

---
+++ app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VoltaicLife::Application.config.secret_key_base = '1993dade60d32fe2a49f9b3577910faf75ae98e1b7bc133bb579493efaa0989599c2bb1e41cce7f97b739b5885af770ff26d21511e808c4b8a0ba9c4e3bf2a06'
