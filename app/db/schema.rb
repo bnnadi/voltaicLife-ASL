5d724b398e8e6ec5a8273748602d4f4af789e2f7
Bisike <bisike.nnadi@gmail.com>
2013-09-25 15:54:55 -0400

    fixed the form issue, thank goodness I did not have to restart the enitire project as planned

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -11,13 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130923231045) do
-
-  create_table "searches", force: true do |t|
-    t.string   "artist"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20130923211407) do
 
   create_table "statuses", force: true do |t|
     t.text     "content"
