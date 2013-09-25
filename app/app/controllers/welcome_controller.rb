b10682ef4dbd91d02ff8e2b8c32ad53c648906b4
Bisike <bisike.nnadi@gmail.com>
2013-09-25 17:01:58 -0400

    update

--- app/app/controllers/welcome_controller.rb
+++ app/app/controllers/welcome_controller.rb
@@ -2,12 +2,10 @@ require 'net/http'
 
 class WelcomeController < ApplicationController
   def index
-    # API_Key = 'xbg5na4bnkpmyqs5f7zdbrnn'
-    # uriEvents = URI('http://api.jambase.com/events?')
-    # uriArtist = URI('http://api.jambase.com/artists?')
-    # uriVenue = URI('http://api.jambase.com/venue?')
   end
   def search
-    
+    JamBase4R.configure do |config|
+     config.api_key = "xbg5na4bnkpmyqs5f7zdbrnn"
+    end
   end
 end
